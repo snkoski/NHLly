f09083dfb02870189650b3d35b18c4f079aa52c2
Shawn Koski <shawnnkoski@gmail.com>
2022-10-10 00:33:58 -0700

    types for stats and info

--- types/types.ts
+++ types/types.ts
@@ -5,8 +5,17 @@ export type Error = {
 };
 
 export type Team = {
+  abbreviation: string;
+  conference: Conference;
+  division: Division;
+  firstYearOfPlay: string;
   id: string;
+  locationName: string;
   name: string;
+  officialSiteUrl: string;
+  shortName: string;
+  teamName: string;
+  venue: Venue;
 };
 
 export type Player = {
@@ -16,12 +25,20 @@ export type Player = {
 };
 
 export type Person = {
+  alternateCaptain: boolean;
   birthDate: string;
+  captain: boolean;
+  currentAge: string;
+  currentTeam: Team;
   fullName: string;
   height: string;
   id: number;
   link: string;
+  nationality: string;
   primaryNumber: string;
+  primaryPosition: Position;
+  rookie: boolean;
+  shootsCatches: string;
   weight: string;
 };
 
@@ -30,4 +47,107 @@ export type Position = {
   code: string;
   name: string;
   type: string;
+};
+
+type Venue = {
+  city: string;
+  id: number;
+  name: string;
+};
+
+type Conference = {
+  id: number;
+  name: string;
+};
+
+type Division = {
+  id: number;
+  name: string;
+  nameShort: string;
+};
+
+export type TeamStats = {
+  rankings: TeamRankings;
+  stats: TeamCountingStats;
+  type: TeamStatsType;
+};
+
+type TeamStatsType = {
+  displayName: string;
+  gameType: GameType;
+};
+
+type GameType = {
+  description: string;
+  id: string;
+  postSeason: boolean;
+};
+
+type TeamCountingStats = {
+  evGGARatio: number;
+  faceOffWinPercentage: string;
+  faceOffsLost: number;
+  faceOffsTaken: number;
+  faceOffsWon: number;
+  gamesPlayed: number;
+  goalsAgainstPerGame: number;
+  goalsPerGame: number;
+  losses: number;
+  ot: number;
+  penaltyKillPercentage: string;
+  powerPlayGoals: number;
+  powerPlayGoalsAgainst: number;
+  powerPlayOpportunities: number;
+  powerPlayPercentage: string;
+  ptPctg: string;
+  pts: number;
+  savePctg: number;
+  shootingPctg: number;
+  shotsAllowed: number;
+  shotsPerGame: number;
+  winLeadFirstPer: number;
+  winLeadSecondPer: number;
+  winOppScoreFirst: number;
+  winOutshootOpp: number;
+  winOutshotByOpp: number;
+  winScoreFirst: number;
+  wins: number;
+};
+
+type TeamRankings = {
+  evGGARatio: string;
+  faceOffWinPercentage: string;
+  faceOffsLost: string;
+  faceOffsTaken: string;
+  faceOffsWon: string;
+  goalsAgainstPerGame: string;
+  goalsPerGame: string;
+  losses: string;
+  ot: string;
+  penaltyKillOpportunities: string;
+  penaltyKillPercentage: string;
+  powerPlayGoals: string;
+  powerPlayGoalsAgainst: string;
+  powerPlayOpportunities: string;
+  powerPlayPercentage: string;
+  ptPctg: string;
+  pts: string;
+  savePctRank: string;
+  shootingPctRank: string;
+  shotsAllowed: string;
+  shotsPerGame: string;
+  winLeadFirstPer: string;
+  winLeadSecondPer: string;
+  winOppScoreFirst: string;
+  winOutshootOpp: string;
+  winOutshotByOpp: string;
+  winScoreFirst: string;
+  wins: string;
+};
+
+export type PlayerSeason = {
+  league: any;
+  season: any;
+  stat: any;
+  team: any;
 };
